00e5eeb633d8de6a1f8beb16d9636abcc41e9273
John Suresh <johnsuresh0206@gmail.com>
2026-04-09 22:57:20 +0530

    message

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,16 @@
+console.log("Hello world!!!");
+console.log("Hello world!!!");
+console.log("Hello world!!!");
+console.log("Hello world!!!");
+console.log("Hello world!!!");
+sumOfNumbers(5, 10);
+console.log("Hello world!!!");
+console.log("Hello world!!!");
+console.log("Hello world!!!");
+console.log("Hello world!!!");
+
+
+//Logic to get the sum of 2 nos
+function sumOfNumbers(a, b)
+  c=a-b;
+  console.log(c);
