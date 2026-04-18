9342b8d319b403a2384b5de31b17ce0b5c6f17b4
John Suresh <johnsuresh0206@gmail.com>
2026-04-18 20:13:42 +0530

    Assignment 1

---
+++ assignments/assignment1.ts
@@ -0,0 +1,76 @@
+//Temperature of a city in degrees Celsius: 25.5
+let temprature: number = 25.5;
+console.log(`Temperature of a city in degrees Celsius: ${temprature}`);
+
+//Whether a customer has placed an order: true or false
+let order: boolean = true;
+console.log(`Whether a customer has placed an order: ${order}`);
+
+//Person's phone number "123-456-7890"
+
+let phonenumber: string = "123-456-7890";
+console.log(`Person's phone number: ${phonenumber}`);
+
+//Amount of money in customer's bank account: 1000.50
+let amount: number = 1000.5;
+console.log(`Amount of money in customer's bank account:${amount}`);
+
+//Person's email address: "john.doe@example.com"
+let email: string = "john.doe@example.com";
+console.log(`Person's email address: ${email}`);
+
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let latitude: number = 37.7749;
+let longitude: number = -122.4194;
+console.log(
+  `Coordinates of a location (latitude, longitude): ${latitude}, ${longitude}`,
+);
+
+//Person's marital status: true or false
+let marital_status: boolean = true;
+console.log(`Person's marital status: ${marital_status}`);
+
+//Person's occupation: "Software Engineer"
+let occupation: string = "Software Engineer";
+console.log(`Person's occupation: ${occupation}`);
+
+//Person's favourite colour: "Blue"
+
+let favourite: string = "Blue";
+console.log(`Person's favourite colour: ${favourite}`);
+
+//Current year: 2023
+let current_year: number = "2023";
+console.log(`Current year: ${current_year}`);
+
+//Number of followers on a social media platform: 1,000,000
+let followers: string = "1,000,000";
+console.log(`Number of followers on a social media platform: ${followers}`);
+
+//Rating of a movie: 7.5
+let rating: number = 7.5;
+console.log(`Rating of a movie: ${rating}`);
+
+//Person's blood type: 'A'
+let blood_type: string = "A";
+console.log(`Person's blood type: ${blood_type}`);
+
+//Title of a book: "To Kill a Mockingbird"
+let book_title: string = "To Kill a Mockingbird";
+console.log(`Title of book: ${book_title}`);
+
+//Number of employees in a company: 500
+let no_of_employees: number = 500;
+console.log(`Number of employees in a company: ${no_of_employee}`);
+
+//Time of an event: 2:30 PM
+let event_time: string = "2.30 PM";
+console.log(`Time of an event: ${event_time}`);
+
+//Name of a country: "United States"
+let country_name: string = "United States";
+console.log(`Name of the country: ${country_name}`);
+
+//18. Person's eye color: "Brown"
+let eye_color: string = "Brown";
+console.log(`Person's eye color: ${eye_color}`);
